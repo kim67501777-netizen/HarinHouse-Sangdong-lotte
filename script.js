690c49ca626848df752ebebf90082a78b172f946
kim67501777-netizen <kim67501777@gmail.com>
2026-08-01 14:51:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
 
+    console.log("HARIN HOUSE 홈페이지 정상 작동");
+
+    // 관심고객 버튼 클릭 안내
+    const buttons = document.querySelectorAll(".gold");
+
+    buttons.forEach(function(button){
+
+        button.addEventListener("click", function(){
+
+            console.log("관심고객 등록 페이지 이동");
+
+        });
+
+    });
+
+});
